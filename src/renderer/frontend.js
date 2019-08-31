acd94520a8bdf4d06c4596d1bc5494282a5ae641
Efrain <efrainbond@hotmail.com>
2019-08-31 13:48:11 -0500

    Creada funcion del buscador/ error this es indefinido

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,9 @@
+const url = require('url')
+const path = require('path')
+
 window.addEventListener('load', () => {
   addImagesEvents()
+  searchImageEvent()
 })
 
 
@@ -22,5 +26,39 @@ function changeImage(node) {
   // Agregamos la clase selected al li que recivimos por parametro que sera el que esta presionado acualmente
   node.classList.add('selected');
 
+  // Seleccionamos la imagen que esta siendo mostrada y cambia su fuente por la imagen seleccionada
   document.getElementById('image-displayed').src = node.querySelector('img').src
+}
+
+function searchImageEvent() {
+  
+  const searchBox = document.getElementById('search-box')
+
+  searchBox.addEventListener('keyup', () => {
+    const regex = new RegExp(this.value.toLowerCase(), 'gi')
+
+    if (this.value.length > 0) {
+      const thumbs = document.querySelectorAll('li.list-group-item img')
+
+      for (let i = 0, length2 = thumbs.length; i < length2; i++) {
+        const fileURL = url.parse(thumbs[i].src)
+        const fuleName = path.basename(fileURL.pathname)
+
+        if (fileName.match(regex)) {
+          thumbs[i].parentNode.classList.remove('hidden')
+        } else {
+          thumbs[i].parentNode.classList.add('hidden')
+        }
+
+      }
+
+      selectFirstImage()
+
+    }
+  })
+}
+
+function selectFirstImage() {
+  const image = document.querySelector('li.list-group-item:not(.hidden)')
+  changeImage(image)
 }
